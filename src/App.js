2f0c43153c9aac1c0a5e2d86b32233f49a4bb38a
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-06 16:23:29 +0100

    styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Item from './Items'
 
 export default function App(){
+    const [list, setList] = useState([])
+    const [item, setItem] = useState('')
+
+    function handleSubmit(e){
+        e.preventDefault()
+        setList(prev => {
+            return [prev, item]
+        })
+        console.log(item)
+    }
+    console.log(list)
+
     return <div className='card'>
+        {/* <p className='alert'>item deleted</p> */}
         <h1 className='title'>Grocery Bud</h1>
-        <form className='form'>
-            <input type='text' placeholder='e.g yam'/>
-            <button type='submit' className='submit'>Submit</button>
+        <form className='form' onSubmit={handleSubmit}>
+            <input type='text' placeholder='e.g yam' value={item} onChange={(e)=>{
+                setItem(e.target.value)}}/>
+            <button type='submit' className='submit' >Submit</button>
         </form>
-        <Item/>
-        <button className='clear'>Clear Items</button>
+        {/* <Item/> */}
+        {/* <button className='clear'>Clear Items</button> */}
     </div>
 }
