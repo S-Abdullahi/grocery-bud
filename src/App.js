658f170e2971df9865f08a7470ef2a7a303394ac
Abdullahi Salawu <salabdullahi33@gmail.com>
2023-02-06 18:44:56 +0100

    functionalities

--- src/App.js
+++ src/App.js
@@ -1,28 +1,69 @@
-import React, {useState} from 'react';
-import Item from './Items'
+import React, { useState } from "react";
+import Item from "./Items";
 
-export default function App(){
-    const [list, setList] = useState([])
-    const [item, setItem] = useState('')
+export default function App() {
+  const [list, setList] = useState([]);
+  const [item, setItem] = useState("");
+  const [clear, setClear] = useState(false);
+  const [edit, setEdit] = useState(false)
+  let alertText = "hello";
 
-    function handleSubmit(e){
-        e.preventDefault()
-        setList(prev => {
-            return [prev, item]
-        })
-        console.log(item)
+  function handleSubmit(e) {
+    e.preventDefault();
+    item === ""
+      ? (alertText = "enter item")
+       : setList((prev) => {
+          return [...prev, item];
+        });
+    setItem("");
+    setClear(true);
+  }
+
+  function clearList() {
+    setList([]);
+    setClear(false);
+  }
+
+  function deleteItem(ind) {
+    const newList = list.filter((_, index) => index !== ind);
+    setList(newList);
+    if(newList.length === 0){
+        setClear(false)
     }
-    console.log(list)
+  }
 
-    return <div className='card'>
-        {/* <p className='alert'>item deleted</p> */}
-        <h1 className='title'>Grocery Bud</h1>
-        <form className='form' onSubmit={handleSubmit}>
-            <input type='text' placeholder='e.g yam' value={item} onChange={(e)=>{
-                setItem(e.target.value)}}/>
-            <button type='submit' className='submit' >Submit</button>
-        </form>
-        {/* <Item/> */}
-        {/* <button className='clear'>Clear Items</button> */}
+  function editItem(ind){
+    const editBud = list.find((_, index) => index === ind)
+    setItem(editBud)
+    setEdit(true)
+  }
+
+  return (
+    <div className="card">
+      <p className="alert">{alertText}</p>
+      <h1 className="title">Grocery Bud</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="e.g yam"
+          value={item}
+          onChange={(e) => {
+            setItem(e.target.value);
+          }}
+        />
+        <button type="submit" className="submit">
+          {edit ? 'Edit' : 'submit'}
+        </button>
+      </form>
+      {list.map((bud, index) => {
+        return <Item key={index} bud={bud} index={index} delete={deleteItem} edit={editItem}/>;
+      })}
+      {clear && (
+        <button className="clear" onClick={() => clearList()}>
+          Clear Items
+        </button>
+      )}
+      {/* <button className='clear'>Clear Items</button> */}
     </div>
+  );
 }
--- src/Items.js
+++ src/Items.js
@@ -2,13 +2,13 @@ import React from "react";
 import {BiEdit} from 'react-icons/bi'
 import {AiFillDelete} from 'react-icons/ai'
 
-export default function Item(){
+export default function Item(props){
     return (
         <div className="item">
-            <p>Yam</p>
+            <p>{props.bud}</p>
             <div className="icon-con">
-                <span className="edit"><BiEdit/></span>
-                <span className="delete"><AiFillDelete/></span>
+                <span className="edit" onClick={()=>props.edit(props.index)}><BiEdit/></span>
+                <span className="delete" onClick={()=>props.delete(props.index)}><AiFillDelete/></span>
             </div>
         </div>
     )
